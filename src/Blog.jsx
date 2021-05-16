012c411c18e8b821b803aa238bb3050169329bfe
島崎塁 <mitsuba19930328@gmail.com>
2021-05-16 17:03:20 +0900

    React hooksの試用

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,63 +3,50 @@ import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isPublished: false,
-            count: 0
-        }
-    }
+const Blog = () => {
 
-    componentDidMount() {
-        // ボタンがクリックされたらいいねを行う
-        document.getElementById('counter').addEventListener('click', this.countUp)
-    }
+    // componentDidMount() {
+    //     // ボタンがクリックされたらいいねを行う
+    //     document.getElementById('counter').addEventListener('click', this.countUp)
+    // }
+    //
+    // componentDidUpdate() {
+    //     if(this.state.count >= 10){
+    //         this.setState({
+    //             count: 0
+    //         })
+    //     }
+    // }
+    //
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp)
+    // }
 
-    componentDidUpdate() {
-        if(this.state.count >= 10){
-            this.setState({
-                count: 0
-            })
-        }
-    }
+    return(
+        <>
+            <Article
+                title = {'Reactの使い方'}
+                // count = {this.state.count}
+            />
+            <FooBar.Foo />
+            <FooBar.Bar />
+            <Hoge />
+        </>
+    )
 
-    componentWillUnmount() {
-        document.getElementById('counter').removeEventListener('click', this.countUp)
-    }
+    // // 公開状態を反転させる関数メソッド
+    // togglePublished = () =>{
+    //     this.setState({
+    //         isPublished: !this.state.isPublished
+    //     })
+    // };
 
-    render(){
-        const authorName = 'テストさん'
-        return(
-            <>
-                <Article
-                    title = {'Reactの使い方'}
-                    isPublished = {this.state.isPublished}
-                    toggle={() => this.togglePublished()}
-                    count = {this.state.count}
-                    toggleLike = {() => this.countUp()}
-                />
-                <FooBar.Foo />
-                <FooBar.Bar />
-                <Hoge />
-            </>
-        )
-    }
-
-    // 公開状態を反転させる関数メソッド
-    togglePublished = () =>{
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-    };
-
-    // 公開状態を反転させる関数メソッド
-    countUp = () =>{
-        this.setState({
-            count: this.state.count + 1
-        })
-    };
+    // // 公開状態を反転させる関数メソッド
+    // countUp = () =>{
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // };
 }
 
 
